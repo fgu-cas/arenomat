b1b968537956be1dd9ed3448682add6ec5b52d62
Michal Vondracek <info@michalvondracek.cz>
2014-05-13 00:57:36 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-sudo apt-get update
-sudo apt-get -y install libopencv-dev nodejs-legacy npm mc libasound2-dev git subversion
-sudo npm config set registry http://registry.npmjs.org/ 
-sudo npm install -g node-gyp 
-npm cache clean -f
-sudo npm install -g n 
-sudo n stable 
+apt-get update 
+apt-get -y --download-only dist-upgrade 
+apt-get -y dist-upgrade
 
+sudo apt-get -y install git libopencv-dev nodejs-legacy npm libasound2-dev alsa-utils mongodb
+sudo npm config set registry http://registry.npmjs.org/ 
+
+npm cache clean -f sudo 
+
+npm install -g n
+sudo n stable 
+sudo npm install -g node-gyp 
+
+git clone http://git.michalvondracek.cz/arenomat.git
+cd arenomat
+npm install
