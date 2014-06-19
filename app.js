17989aca55d28a3ee0a1d156eda2b50c2b278b55
Majkl <info@michalvondracek.cz>
2014-06-18 23:03:14 -0400

    UI

--- app.js
+++ app.js
@@ -54,6 +54,19 @@ fs.readdirSync(modelsPath).forEach(function(file) {
 var routes = require('./routes');
 var arena = require('./lib/arena');
 
+//mongoose
+//mongoose.set('debug', true)
+var URI = "mongodb://localhost/arenomat";
+mongoose.connect(URI, function(err) {
+  if (err) {
+    throw err;
+  }
+  return console.log('Connected to database');
+});
+
+var Frame = mongoose.model('Frame');
+var Session = mongoose.model('Session');
+
 // Set View Engine
 app.engine('html', require('hogan-express'));
 app.set('view options', {layout: true});
@@ -79,18 +92,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 
-//mongoose
-//mongoose.set('debug', true)
-var URI = "mongodb://localhost/arenomat";
-mongoose.connect(URI, function(err) {
-  if (err) {
-    throw err;
-  }
-  return console.log('Connected to database');
-});
-
-var Frame = mongoose.model('Frame');
-var Session = mongoose.model('Session');
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -123,6 +124,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
+http.listen(80, function() {
+  console.log('Listening on port %d', http.address().port);
+});
 
 
 
@@ -267,9 +271,6 @@ board.on("ready", function() {
   });
 });
 
-http.listen(80, function() {
-  console.log('Listening on port %d', http.address().port);
-});
 
 if (vc) {
   isWebcam = true;
