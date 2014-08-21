b96c8091346850a7079f00b22e23d9d4d18ed9d8
Your Name <you@example.com>
2014-08-21 04:35:19 +0200

    uvcdynctrl

--- install/install.sh
+++ install/install.sh
@@ -1,1 +1,3 @@
-sudo apt-get -y install git libopencv-dev nodejs-legacy npm libasound2-dev alsa-utils mongodb libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
+sudo apt-get -y install git libopencv-dev nodejs-legacy npm libasound2-dev alsa-utils mongodb libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++ uvcdynctrl v4l-utils
+uvcdynctrl -i /usr/share/uvcdynctrl/data/046d/logitech.xml
+
