294ccfc3f3ec78fa2192c3489fef396568e74041
Michal Vondracek <info@michalvondracek.cz>
2015-11-10 02:01:35 +0100

    install script, pm2 startup

--- install/install.sh
+++ install/install.sh
@@ -2,13 +2,15 @@ sudo apt-get -y install git libopencv-dev nodejs-legacy npm libasound2-dev alsa-
 uvcdynctrl -i /usr/share/uvcdynctrl/data/046d/logitech.xml
 
 npm config set registry http://registry.npmjs.org/ 
-npm install -g node-gyp 
 npm cache clean -f 
 npm install -g n 
 n 0.12.7 
+npm install -g node-gyp pm2 
 
 mkdir /data
 cd /data
 git clone https://github.com/fgu-cas/arenomat
 cd arenomat
-cp ./install/arenomat.conf /etc/init
+pm2 startup
+pm2 start app.js
+pm2 save
